d63920fcc93c36770394ec4fdd2de4c79a0ae864
Keyur-Padsada <108683491+Keyur-Padsada@users.noreply.github.com>
2022-11-29 19:16:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function aler(){
+    // const bar = document.getElementById("bar");
+    const nav = document.getElementById("navbar");
+    nav.classList.add("myStyle")
+   
+    
+   
+    
+}
+  
+
